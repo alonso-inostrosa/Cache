ba8384cea8b248ed1879e3a3bc3f3cd3169a192d
Alonso <alonso.inostrosa@gmail.com>
2021-12-10 16:14:48 -0300

    Use of std::hash

--- cache_items.h
+++ cache_items.h
@@ -1,7 +1,7 @@
 #ifndef CACHE_ITEMS_H
 #define CACHE_ITEMS_H
 
-#include<string>
+#include<iostream>
 
 class CacheItem{
 public:
@@ -11,6 +11,12 @@ public:
     CacheItem( int priority, std::string key ){
         this->priority = priority;
         this->key = key;
+    }
+
+    friend std::ostream& operator << (std::ostream& os, const CacheItem& ci)
+    {
+        os << std::to_string(ci.priority) << "|" << ci.key;
+        return os;
     }
 
     friend bool operator<(CacheItem& a, CacheItem& b)  {
--- main.cc
+++ main.cc
@@ -2,6 +2,23 @@
 #include "cache_items.h"
 
 #include <iostream>
+#include <string>
+#include <iomanip>
+
+//Para usar Hash sobre objetos CacheItem (inserta la función en espacio std)
+namespace std{
+    template<> struct hash<CacheItem>{
+        std::size_t operator()(CacheItem const& ci) const noexcept
+        {
+            std::size_t h1 = std::hash<int>{}(ci.priority);
+            std::size_t h2 = std::hash<std::string>{}(ci.key);
+            return h1 ^ (h2 << 1); 
+        }
+    };
+}
+
+//Para usar hash
+#include <functional>
 
 int main(int argc, char* argv[] ){
 
@@ -11,6 +28,10 @@ int main(int argc, char* argv[] ){
     std::shared_ptr<CacheItem> c_item_3a = std::make_shared<CacheItem>( 3, "tres" );
     std::shared_ptr<CacheItem> c_item_3b = std::make_shared<CacheItem>( 3, "tres" );
     std::shared_ptr<CacheItem> c_item_3c = std::make_shared<CacheItem>( 3, "tres" );
+
+    std::hash<CacheItem> hs;
+
+    std::cout << "hash(" << *c_item_1 << ")=" << hs( *c_item_1 ) << std::endl;
 
     Cache cache;
     cache.insert( c_item_3c );
